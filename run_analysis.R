8f31704d6775ec8328fce38bd466a748b391efd6
Santos-Valera <53722096+Santos-Valera@users.noreply.github.com>
2019-08-30 17:22:39 +0200

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -17,11 +17,11 @@ files<-list.files(path_rf, recursive=TRUE)
 
 dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
 dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
-Read the Subject files
+
 
 dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
 dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
-Read Fearures files
+
 
 dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
 dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
@@ -47,7 +47,7 @@ Data <- cbind(dataFeatures, dataCombine)
 #i.e taken Names of Features with ???mean()??? or ???std()???
 
 subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
-Subset the data frame Data by seleted names of Features
+
 selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
 Data<-subset(Data,select=selectedNames)
 
@@ -64,12 +64,9 @@ names(Data)<-gsub("^f", "frequency", names(Data))
 names(Data)<-gsub("Acc", "Accelerometer", names(Data))
 names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
 names(Data)<-gsub("Mag", "Magnitude", names(Data))
-names(Data)<-gsub("BodyBody", "Body", names(Data))
 
-# Creates a second,independent tidy data set and ouput it
-# In this part,a second, independent tidy data set will be created with the average of each variable for each activity and each subject based on the data set in step 4.
+# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-library(plyr);
 Data2<-aggregate(. ~subject + activity, Data, mean)
 Data2<-Data2[order(Data2$subject,Data2$activity),]
 write.table(Data2, file = "tidydata.txt",row.name=FALSE)
